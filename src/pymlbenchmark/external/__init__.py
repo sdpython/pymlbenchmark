49c242bd6cdae2ce5ad65dd91b95bb618150849a
xavier dupré <xavier.dupre@gmail.com>
2019-07-15 14:32:34 +0200

    fix internal import

--- src/pymlbenchmark/external/__init__.py
+++ src/pymlbenchmark/external/__init__.py
@@ -4,6 +4,7 @@
 """
 
 from .onnxruntime_perf_binclass import OnnxRuntimeBenchPerfTestBinaryClassification
+from .onnxruntime_perf_regression import OnnxRuntimeBenchPerfTestRegression
 from .onnxruntime_perf_list import (
     onnxruntime_perf_binary_classifiers,
     onnxruntime_perf_regressors,
